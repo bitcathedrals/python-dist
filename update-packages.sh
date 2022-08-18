61fa9d1874667227f4a3b02b9f3ac81d0ddfae57
Michael Mattie <michael.mattie.employers@gmail.com>
2022-08-18 11:08:03 -0700

    add a human browser

--- update-packages.sh
+++ update-packages.sh
@@ -13,21 +13,37 @@ do
   <body>
 HEAD
 
+   cat >${project}-browse.html <<HEAD
+<!DOCTYPE html>
+<html>
+  <h1>${project}</h1>
+  <body>
+    <ul>
+HEAD
+
     for file in $(find ${subdir} -type f -depth 1 -print)
     do
       sha256=`sha256sum ${file} | tr -s ' ' | cut -d ' ' -f 1`
       name=`basename ${file}`
 
       echo >>${project}.html "<a href=\"/simple/${project}/${name}#sha256=${sha256}\">${name}</a>"
-    done
 
+      echo >>${project}-browse.html "<li><a href=\"http://bitcathedrals.com/simple/${project}/${name}\">${name}</a></li>"
+    done
 
     cat >>${project}.html <<TAIL
   </body>
 </html>
 TAIL
 
+   cat >>${project}-browse.html <<TAIL
+    </ul>
+  </body>
+</html>
+TAIL
+
     aws s3 cp ${project}.html "s3://bitcathedrals.com/simple/${project}/index.html" --profile root
+    aws s3 cp ${project}-browse.html "s3://bitcathedrals.com/simple/${project}/browse.html" --profile root
 done
 
 
--- update-projects.sh
+++ update-projects.sh
@@ -6,11 +6,26 @@ cat >packages.html <<HEAD
   <body>
 HEAD
 
+cat >packages-browse.html <<HEAD
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>bitcathedrals.com Python Package Repository</h1>
+
+    <p>
+      repository URL = http://bitcathedrals.com/simple , with trusted-host = bitcathedrals.com
+    </p>
+
+    <ul>
+HEAD
+
 for project in $(ls remote)
 do
   repo_path=`basename "${project}"`
 
   echo "<a href=\"/${repo_path}/\">${repo_path}</a>" >>packages.html
+
+  echo "<li><a href=\"http://bitcathedrals.com/simple/${repo_path}/browse.html\">${repo_path}</a></li>" >>packages-browse.html
 done
 
 cat >>packages.html <<TAIL
@@ -18,4 +33,11 @@ cat >>packages.html <<TAIL
 </html>
 TAIL
 
+cat >>packages-browse.html <<TAIL
+    </ul>
+  </body>
+</html>
+TAIL
+
 aws s3 cp packages.html s3://bitcathedrals.com/simple/index.html --profile root
+aws s3 cp packages-browse.html s3://bitcathedrals.com/simple/browse.html --profile root
